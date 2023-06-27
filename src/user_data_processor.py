9ea6453c0e839e1740cbc57fa36b94a26049bc7b
Henrik Élő <henrikeloe@gmail.com>
2023-06-27 20:34:26 +0200

    Refactor lambda code

--- src/user_data_processor.py
+++ src/user_data_processor.py
@@ -7,46 +7,53 @@ import uuid
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-endpoint_url = f"http://{os.environ.get('LOCALSTACK_HOSTNAME')}:os.environ.get('EDGE_PORT')" if os.environ.get('LOCALSTACK_HOSTNAME', None) else None
+endpoint_url = f"http://{os.environ.get('LOCALSTACK_HOSTNAME')}:{os.environ.get('EDGE_PORT')}" if os.environ.get('LOCALSTACK_HOSTNAME', None) else None
 dynamodb =  boto3.resource('dynamodb', region_name=os.environ.get('AWS_REGION'), endpoint_url=endpoint_url)
 s3_client = boto3.client('s3', region_name=os.environ.get('AWS_REGION'), endpoint_url=endpoint_url)
 
 def process_new_users(event, _):
-    if "Records" not in event:
-        logger.error("No 'Records' found in event")
-        raise ValueError(("Event does not have key named 'Records'"))
-    
-    users_to_write = []
-    cars_to_write = []
+    users_to_write_to_db = []
+    cars_to_write_to_db = []
 
+    logger.info(f"Collecting data to write into database")
     for record in event["Records"]:
-        bucket = record["s3"]["bucket"]["name"]
-        s3_file_key = record["s3"]["object"]["key"]
-        logger.info(f"bucket: {bucket} key: {s3_file_key}")
-        user_file = s3_client.get_object(Bucket=bucket, Key=s3_file_key)
-        logger.info(f"Got file: {user_file}")
-        users = json.loads(user_file["Body"].read())
-        logger.info(f"Loaded file: {user_file}")
+        users = get_users_from_record(record)
         for user in users:
-            car = user["car"]
-            car_uuid = str(uuid.uuid4())
-            user["car"] = car_uuid
-            car["id"] = car_uuid
-            logger.info(car)
-            logger.info(user)
-            users_to_write.append(user)
-            cars_to_write.append(car)
+            car, user = create_car_and_user_db_items(user)
+            users_to_write_to_db.append(user)
+            cars_to_write_to_db.append(car)
+    logger.info(f"Data collection complete")
 
-    logger.info(users_to_write)
-    logger.info(cars_to_write)
+    try:
+        logger.info(f"Staring database write")
+        write_users_and_cars_to_db(users_to_write_to_db, cars_to_write_to_db)
+        logger.info(f"Database write successful")
+    except Exception as err:
+        logger.error(f"Couldn't save data to database. Error: {err}", )
+        raise
 
+def get_users_from_record(record):
+    bucket = record["s3"]["bucket"]["name"]
+    s3_file_key = record["s3"]["object"]["key"]
+    logger.info(f"bucket: {bucket} key: {s3_file_key}")
+    user_file = s3_client.get_object(Bucket=bucket, Key=s3_file_key)
+    users = json.loads(user_file["Body"].read())
+    return users
+
+def create_car_and_user_db_items(user):
+    car = user["car"]
+    car_uuid = str(uuid.uuid4())
+    user["car"] = car_uuid
+    car["id"] = car_uuid
+    return car, user
+    
+def write_users_and_cars_to_db(users_to_write, cars_to_write):
     user_table = dynamodb.Table(os.environ.get("user_db_name"))
     car_table = dynamodb.Table(os.environ.get("car_db_name"))
     
     with user_table.batch_writer() as user_writer:
         with car_table.batch_writer() as car_writer:
             for user in users_to_write:
-                user_writer.put_item(user)
+                user_writer.put_item(Item=user)
             for car in cars_to_write:
-                car_writer.put_item(car)
-    
+                car_writer.put_item(Item=car)
