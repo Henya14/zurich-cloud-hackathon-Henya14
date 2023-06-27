e6ed9e22ad685cfbe27f64ba1c7019683c4ffe85
Henrik Élő <henrikeloe@gmail.com>
2023-06-27 20:04:44 +0200

    Remove non-indexed attributes

--- dynamodb.tf
+++ dynamodb.tf
@@ -9,37 +9,6 @@ resource "aws_dynamodb_table" "user_table" {
       name = "id"
       type = "S"
     }
-
-    attribute {
-      name = "name"
-      type = "S"
-    }
-
-    attribute {
-      name = "surname"
-      type = "S"
-    }
-
-    attribute {
-      name = "birthdate"
-      type = "S"
-    }
-
-    attribute {
-      name = "address"
-      type = "S"
-    }
-
-    attribute {
-      name = "car"
-      type = "S"
-    }
-
-    attribute {
-      name = "fee"
-      type = "N"
-    }
-
 }
 
 
@@ -49,50 +18,9 @@ resource "aws_dynamodb_table" "car_table" {
     read_capacity = 5
     write_capacity = 5
     hash_key = "id"
-    
+
     attribute {
       name = "id"
       type = "S"
     }
-
-    attribute {
-      name = "make"
-      type = "S"
-    }
-
-    attribute {
-      name = "model"
-      type = "S"
-    }
-
-    attribute {
-      name = "year"
-      type = "N"
-    }
-
-    attribute {
-      name = "color"
-      type = "S"
-    }
-
-    attribute {
-      name = "plate"
-      type = "S"
-    }
-
-    attribute {
-      name = "mileage"
-      type = "N"
-    }
-
-    attribute {
-      name = "fuelType"
-      type = "S"
-    }
-
-    attribute {
-      name = "transmission"
-      type = "S"
-    }
-
 }
